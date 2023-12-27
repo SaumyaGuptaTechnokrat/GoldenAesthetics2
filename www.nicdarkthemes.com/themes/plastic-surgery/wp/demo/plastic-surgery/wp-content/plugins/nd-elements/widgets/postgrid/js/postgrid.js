20073d63c5389dc58ca8d048a76ed321ef2b29e0
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-12-27 23:24:53 +0530

    First Commit

---
+++ www.nicdarkthemes.com/themes/plastic-surgery/wp/demo/plastic-surgery/wp-content/plugins/nd-elements/widgets/postgrid/js/postgrid.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function() {
+
+        //START masonry
+        jQuery(function($) {
+
+            var $nd_elements_masonry_content = $(".nd_elements_masonry_content").imagesLoaded(function() {
+                $nd_elements_masonry_content.masonry({
+                    itemSelector: ".nd_elements_masonry_item"
+                });
+            });
+
+        });
+        //END masonry
+
+    });
+
+
+})(jQuery);
